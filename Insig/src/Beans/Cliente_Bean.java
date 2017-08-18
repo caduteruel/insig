d1f41cc9276b89fe190f2aecc6d8e338dea50f9b
guilherme250493 <guilherme250493@hotmail.com>
2017-08-17 22:34:10 -0300

    Classes e insert do cliente para cadastro

--- Insig/src/Beans/Cliente_Bean.java
+++ Insig/src/Beans/Cliente_Bean.java
@@ -24,14 +24,24 @@ public class Cliente_Bean {
 
     
     public void teste() throws SQLException{
-        Connection conexao = (Connection) ConexaoDAO.createConnection();
-        java.sql.Statement st = conexao.createStatement();
-	ResultSet rs = st.executeQuery("Select * From Cliente;");
-        while (rs.next()) {
-            cliente = new Cliente(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getString(7));
-	}
-        System.out.println("nome " + cliente.getNome() + " cel " + cliente.getCelular());
+        try (Connection conexao = (Connection) ConexaoDAO.createConnection()) {
+            java.sql.Statement st = conexao.createStatement();
+            ResultSet rs = st.executeQuery("Select * From Cliente;");
+            while (rs.next()) {
+                cliente = new Cliente(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getString(7));
+            }
+            System.out.println("nome " + cliente.getNome() + " cel " + cliente.getCelular());
+        }
         
     }
+    
+    public void InserCliente(Cliente c) throws SQLException{
+        try (Connection conexao = (Connection) ConexaoDAO.createConnection()) {
+            java.sql.Statement st = conexao.createStatement();
+            ResultSet rs = st.executeQuery("insert into cliente (nome,cpf,endereco,telefone,celular,sexo) VALUES ('"+c.getNome()+ "','"+c.getCpf()+ "','"+c.getEndereço()+ "','"+c.getTelefone()+ "','"+c.getCelular()+ "','"+c.getSexo()+"');" );
+        }
+    }
+    
+    
     
 }
